10f48f6179afbb1396b8da64ae80414781f70f77
carsol <csolares23@gmail.com>
2018-02-11 15:27:53 -0800

    More updates

--- asdf/install.sh
+++ asdf/install.sh
@@ -5,4 +5,8 @@
 if [ ! -d ~/.asdf ]; then
   echo "installing asdf..."
   git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.4.1
+
+  asdf plugin-add nodejs
+  asdf plugin-add ruby
+  # asdf install
 fi
