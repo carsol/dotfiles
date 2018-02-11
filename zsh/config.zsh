960b19e94a038b85336a6076fb22c3da22520ea0
carsol <csolares23@gmail.com>
2018-02-10 23:31:54 -0800

    install oh my zsh and more changes

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -1,9 +1,3 @@
-export LSCOLORS="exfxcxdxbxegedabagacad"
-export CLICOLOR=true
-
-fpath=($ZSH/functions $fpath)
-
-autoload -U $ZSH/functions/*(:t)
 
 HISTFILE=~/.zsh_history
 HISTSIZE=10000
@@ -30,10 +24,3 @@ setopt HIST_REDUCE_BLANKS
 # don't expand aliases _before_ completion has finished
 #   like: git comm-[tab]
 setopt complete_aliases
-
-bindkey '^[^[[D' backward-word
-bindkey '^[^[[C' forward-word
-bindkey '^[[5D' beginning-of-line
-bindkey '^[[5C' end-of-line
-bindkey '^[[3~' delete-char
-bindkey '^?' backward-delete-char
---
+++ zsh/install.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+#
+# oh-my-zsh installation
+#
+
+# install oh my zsh
+if [ ! -d ~/.oh-my-zsh ]; then
+  echo "installing oh-my-zsh..."
+  git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+fi
+
+# set zsh as default
+chsh -s /bin/zsh
