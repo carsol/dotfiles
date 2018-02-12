a83a93b031a79cc888c0bbd3d763d878563a8b77
carsol <csolares23@gmail.com>
2018-02-11 16:02:41 -0800

    More change.. man oh man

--- asdf/install.sh
+++ asdf/install.sh
@@ -6,7 +6,14 @@ if [ ! -d ~/.asdf ]; then
   echo "installing asdf..."
   git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.4.1
 
-  asdf plugin-add nodejs
-  asdf plugin-add ruby
+  # install ruby
+  # asdf plugin-add ruby
+
+  # install nodejs
+  # asdf plugin-add nodejs
+  # bash ~/.asdf/plugins/nodejs/bin/import-release-team-keyring
+
   # asdf install
+  # npm -g install yarn
+  # asdf reshim nodejs
 fi
