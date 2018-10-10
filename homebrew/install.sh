8cfdfa11fb4d1614bba62a7a7d7492b016cc9f4f
Carlos Solares <csolares23@gmail.com>
2018-10-09 22:38:26 -0700

    Broken down bootstrap script more

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -8,8 +8,18 @@
 # Check for Homebrew
 if test ! $(which brew)
 then
-  echo "  Installing Homebrew for you."
+  echo "Installing homebrew..."
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
+
+# Upgrade homebrew
+echo "brew update..."
+brew update
+
+cd "$(dirname $0)"/..
+
+# Run Homebrew through the Brewfile
+echo "brew bundle..."
+brew bundle
 
 exit 0
